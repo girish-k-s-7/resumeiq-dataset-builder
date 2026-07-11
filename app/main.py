45c254896962b158d1fe65fa852f3539e37306a6
girish-k-s-7 <hemanthkumarr018@gmail.com>
2026-07-11 21:08:24 +0530

    chore: initializae ResumeIQ Dataset Builder Project

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title = "ResumeIQ Dataset Builder",
+    description = "Internal tool for generating anonymized ATS-friendly resumes.",
+    version = "0.1.0",
+)
+
+@app.get("/")
+def root():
+    return{
+        "message": "ResumeIQ Dataset Builder API is running."
+    }
